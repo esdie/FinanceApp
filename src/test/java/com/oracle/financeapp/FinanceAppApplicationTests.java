0be21fd4684d2537eafa43cb02e5bf485304846a
Shreeyash Dixit <Shreeyash Dixit@10.176.139.187>
2023-10-09 10:35:27 +0530

    shreeyash: commit;

--- src/test/java/com/oracle/financeapp/FinanceAppApplicationTests.java
+++ src/test/java/com/oracle/financeapp/FinanceAppApplicationTests.java
@@ -1,7 +1,10 @@
 package com.oracle.financeapp;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.oracle.service.LoanService;
 
 @SpringBootTest
 class FinanceAppApplicationTests {
@@ -9,5 +12,9 @@ class FinanceAppApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
+	
+	@Mock
+	LoanService loanService;
+	
+	
 }
